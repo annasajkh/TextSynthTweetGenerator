fb24c1e6c2fca5644ab49b4edd6a13a27da997ef
Annas <61279556+annasajkh@users.noreply.github.com>
2021-08-30 15:58:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+os.system("pip install -U --upgrade -e git+https://github.com/twintproject/twint.git@origin/master#egg=twint")
+os.system("pip uninstall dataclasses -y")
+
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+
+
+import uvicorn
+import util
+
+
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+  return "Alive"
+
+@app.get("/{username}", response_class=HTMLResponse)
+def generate(username):
+  try:
+    return util.generate_tweet(username)
+  except Exception as e:
+    return f"<p>{e}</p>"
+
+if __name__ == "__main__":
+  uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ util.py
@@ -0,0 +1,55 @@
+from typing import List
+
+
+from twint import output, tweet
+import twint
+import re
+import requests
+import json
+from requests.models import Response
+
+
+def get_tweet(username : str) -> str:
+  config : twint.Config = twint.Config()
+  config.Username = username
+  config.Filter_retweets = True
+  config.Store_object = True
+  config.Hide_output = True
+  config.Limit = 1000
+
+  twint.run.Search(config)
+
+  tweets : List[tweet.tweet] = output.tweets_list
+  result : str = ""
+
+  print(f"scraped: {len(tweets)}")
+
+  for t in tweets:
+    if "@" in t.tweet:
+      continue
+
+    text = re.sub("https://(.*)", "", t.tweet)
+    text = re.sub("\n", " ", text).strip()
+
+    if text.strip() != "":
+      result += f"tweet: {text}\n"
+  
+  return result
+
+def generate_tweet(username : str) -> str:
+  tweet_text : str = get_tweet(username)
+
+  payload : dict  = {
+    "prompt": tweet_text,
+    "temperature": 0.9,
+    "top_k": 40, 
+    "top_p": 0.9, 
+    "seed": 0
+  }
+
+  resp : Response = requests.post("https://bellard.org/textsynth/api/v1/engines/gptj_6B/completions",data=json.dumps(payload, ensure_ascii=False).encode("utf-8"))
+
+  text = filter(lambda x: x != "",[chunk for chunk in resp.text.split("\n")])
+  text = "".join([json.loads(chunk)["text"] for chunk in text]).strip()
+
+  return "\n".join([f"<p>{tweet}</p>" for tweet in text.split("\n")])
